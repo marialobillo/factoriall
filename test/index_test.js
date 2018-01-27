0f3510e8c51d6c3d89a9b8fa90220d8ff81e1405
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-01-27 12:56:27 +0100

    test cases

--- test/index_test.js
+++ test/index_test.js
@@ -3,9 +3,19 @@ var Calculate =  require('../index.js')
 
 describe('Calculate', () => {
   describe('.factorial', () => {
-    it('return the outout for 5!', () => {
+    it('return the output for 5!', () => {
       const expected = Calculate.factorial(5);
       result = 120;
+      assert.equal(expected, result);
+    });
+    it('return the output for 4!', () => {
+      const expected = Calculate.factorial(4);
+      result = 24;
+      assert.equal(expected, result);
+    });
+    it('return the 0!', () => {
+      const expected = Calculate.factorial(0);
+      result = 1;
       assert.equal(expected, result);
     });
   });
