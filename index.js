04849077a8397ba18b03686c3d590012f1a956d5
Maria Lobillo <maria.lobillo.santos@gmail.com>
2018-01-27 12:51:20 +0100

    setup exercise verify

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const Calculate = {
-
+  factorial(num){
+    if(num === 0){
+      return 1;
+    }
+    return num * Calculate.factorial(num - 1);
+  },
 }
 
 module.exports = Calculate;
--- test/index_test.js
+++ test/index_test.js
@@ -3,6 +3,10 @@ var Calculate =  require('../index.js')
 
 describe('Calculate', () => {
   describe('.factorial', () => {
-
+    it('return the outout for 5!', () => {
+      const expected = Calculate.factorial(5);
+      result = 120;
+      assert.equal(expected, result);
+    });
   });
 });
